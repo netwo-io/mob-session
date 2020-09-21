b36d7d9dcbac3639718f6ac8916c15670b03ce0f
Tanguy Godin <ubuntu@desktop-linux-1-vm.europe-west1-b.c.cosmic-mariner-269608.internal>
2020-09-21 10:04:55 +0000

    "mob next [ci-skip]"

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,41 @@ fn main() {
     println!("Hello, world!");
 }
 
+pub fn display_in_a_frame(sentence: &str) -> &str {
+
+    let
+    r#"*********
+* I     *
+* Love  *
+* Netwo *
+*********"#
+}
+
 #[cfg(test)]
 mod test {
+    use super::*;
+
     #[test]
     pub fn it_shall_display_i_love_netwo_in_a_frame() {
-        assert_eq!(display_in_a_frame("I love netwo"), r#"
-            *********
-            * I     *
-            * Love  *
-            * Netwo *
-            *********");
+        assert_eq!(
+            display_in_a_frame("I Love Netwo"),
+            r#"*********
+* I     *
+* Love  *
+* Netwo *
+*********"#
+        );
+    }
+
+    #[test]
+    pub fn it_shall_display_i_love_netwo_in_a_frame_lowercase() {
+        assert_eq!(
+            display_in_a_frame("i love netwo"),
+            r#"*********
+* i     *
+* love  *
+* netwo *
+*********"#
+        );
     }
 }
