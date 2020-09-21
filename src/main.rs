cb800c53942cd5ba5c28d6e1d5e22db3b142e2c8
François-Guillaume Ribreau <github@fgribreau.com>
2020-09-21 12:10:42 +0200

    "mob next [ci-skip]"

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+use std::cmp::max;
+
 fn main() {
     println!("Hello, world!");
 }
 
 pub fn display_in_a_frame(sentence: &str) -> &str {
+    let words: Vec<&str> = sentence.split(' ').collect();
+    let largest_word_length = words.iter().fold(0, |acc, word| max(acc, word.len()));
 
-    let
+
     r#"*********
 * I     *
 * Love  *
